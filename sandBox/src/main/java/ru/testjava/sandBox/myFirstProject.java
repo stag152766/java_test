0e698dea2dbdf0c9c026689c41b617435a0d5f4b
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-16 10:12:29 +0700

    Урок 10

--- sandBox/src/main/java/ru/testjava/sandBox/myFirstProject.java
+++ sandBox/src/main/java/ru/testjava/sandBox/myFirstProject.java
@@ -4,5 +4,9 @@ public class MyFirstProject {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+		int l = 6;
+		int s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
 	}
 }
