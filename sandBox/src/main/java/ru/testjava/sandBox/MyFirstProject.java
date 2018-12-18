d3c0e8b6ba626f57f1e3bf54150eb1f5ac198373
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-18 18:03:35 +0700

    Добавлено несколько функций

--- sandBox/src/main/java/ru/testjava/sandBox/MyFirstProject.java
+++ sandBox/src/main/java/ru/testjava/sandBox/MyFirstProject.java
@@ -3,10 +3,30 @@ package ru.testjava.sandBox;
 public class MyFirstProject {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		int l = 8;
-		int s = l * l;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+		hello("world");
+		hello("user");
+		hello("Max");
 
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len){
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
