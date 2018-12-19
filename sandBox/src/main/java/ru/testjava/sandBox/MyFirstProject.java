97ada75b5c74a09adf43d7f73af847c6e482b20b
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-19 19:14:31 +0700

    Занятие 1 завершено

--- sandBox/src/main/java/ru/testjava/sandBox/MyFirstProject.java
+++ sandBox/src/main/java/ru/testjava/sandBox/MyFirstProject.java
@@ -3,27 +3,21 @@ package ru.testjava.sandBox;
 public class MyFirstProject {
 
 	public static void main(String[] args) {
-		hello("world");
-		hello("user");
-		hello("Max");
 
 		Square s = new Square(5);
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s)); /* передаем объект, в качестве параметра функции
-s.1 используем объект, у которого мы берем атрибут s.l
-*/
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()); // обращаемся к методу, который ассоциирован с объектом
+
 		Rectangle r = new Rectangle(4, 6); // чать когда, где функция вызывается
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
-	}
-
-	public static void hello(String somebody) {
-		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area(Square s){ // функция принимает не сторону квадрата, а объект типа Квадрат
-		return s.l * s.l; // для того чтобы получить доступ к атрибуту объекта, необходимо написать (объект).(значение атрибута)
-	}
-
-	public static double area(Rectangle r) {
-		return r.a * r.b;
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 }
+
+
+
+
+
+
+
+
+
+
--- sandBox/src/main/java/ru/testjava/sandBox/Rectangle.java
+++ sandBox/src/main/java/ru/testjava/sandBox/Rectangle.java
@@ -5,8 +5,12 @@ public class Rectangle {
     public double a;
     public double b;
 
-    public Rectangle(double a, double b){
+    public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
+    }
+
+    public double area() {
+        return this.a * this.b; //ссылка на объект доступно через ключевое слово this. ссылка на тот объект, в котором этот метод вызван
     }
 }
