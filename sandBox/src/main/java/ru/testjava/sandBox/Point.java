3909c4ebe28b8d804012daa5655ad2ec256f0416
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-20 19:02:25 +0700

    Дз2 завершено

---
+++ sandBox/src/main/java/ru/testjava/sandBox/Point.java
@@ -0,0 +1,28 @@
+package ru.testjava.sandBox;
+
+public class Point {
+  public double x1; // Объекты этого класса должны содержать два атрибута
+  public double x2;
+
+
+  public static double distance(Point p1, Point p2) { //функция принимает объекты класса в качестве параметра
+
+
+    p1.x1 = 2.54;
+    p2.x2 = 10.0;
+    return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1));
+  }
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point();
+    Point p2 = new Point();
+
+    System.out.println("Расстояние менжду точками" + p1.x1 + " и " + p2.x2 +"равно " + distance(p1, p2));
+
+
+  }
+
+
+}
+
