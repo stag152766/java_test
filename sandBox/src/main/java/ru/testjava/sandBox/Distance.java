8f16a48d671d17f73ea6a8db3dd958493feff099
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-21 17:57:30 +0700

    дз2 выполнено

---
+++ sandBox/src/main/java/ru/testjava/sandBox/Distance.java
@@ -0,0 +1,19 @@
+package ru.testjava.sandBox;
+
+public class Distance {
+  public double x1;
+  public double x2;
+  public double y1;
+  public double y2;
+
+  public Distance(double x1, double x2, double y1, double y2) {
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+  }
+
+  public double distance() {
+    return Math.sqrt(Math.pow((this.x2 - this.x1), 2)+ Math.pow((this.y2 - this.y1), 2));
+  }
+}
--- sandBox/src/main/java/ru/testjava/sandBox/Point.java
+++ sandBox/src/main/java/ru/testjava/sandBox/Point.java
@@ -1,24 +1,12 @@
 package ru.testjava.sandBox;
 
 public class Point {
-  public double x1; // Объекты этого класса содержат по атрибута
-  public double x2;
-  public double y1;
-  public double y2;
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x1 = 0;
-    p2.x2 = 10.0;
-    p1.y1 = - 4.0;
-    p2.y2 = 5.0;
-    System.out.println("Расстояние между точками (" + p1.x1 + "; " + p1.y1+ ") и (" + p2.x2 + "; " + p2.y2 +  ") равно " + distance(p1, p2));
-  }
+    Distance d = new Distance(2,1,5,7);
+      System.out.println("Расстояние между точками (" + d.x1 + "; " + d.y1+ ") и (" + d.x2 + "; " + d.y2 +  ") равно " + d.distance());
 
-  public static double distance(Point p1, Point p2) { //функция принимает объекты класса в качестве параметра
-    return Math.sqrt(Math.pow((p2.x2 - p1.x1), 2)+ Math.pow((p2.y2 - p1.y1), 2));
   }
 
 }
