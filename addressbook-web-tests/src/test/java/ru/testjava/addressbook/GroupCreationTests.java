24d8284156b7ba47a168542f4a8e53c9a82bff99
stag152766 <45874547+stag152766@users.noreply.github.com>
2018-12-23 08:19:55 +0700

    урок21 модификация
    авторизация перенесена в метод инициализации фикстура

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/GroupCreationTests.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class testUntitledTest {
+public class GroupCreationTests {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -22,10 +22,6 @@ public class testUntitledTest {
     driver = new ChromeDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testUntitledTestCase() throws Exception {
     driver.get("http://localhost/addressbook/edit.php");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
@@ -33,6 +29,10 @@ public class testUntitledTest {
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
