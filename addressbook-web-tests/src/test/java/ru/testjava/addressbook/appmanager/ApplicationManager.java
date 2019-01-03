d5affa0e714d6c7d5909eeb2940d419e5f3ba9e6
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-01-03 09:00:43 +0700

    урок30 в процессе

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,10 @@ package ru.testjava.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
@@ -15,10 +19,24 @@ public class ApplicationManager {
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    System.setProperty("webdriver.chrome.driver", "c:\\Tools\\chromedriver.exe");
-    driver = new ChromeDriver();
+    String browser = BrowserType.CHROME;
+    if (browser == BrowserType.CHROME) {
+      System.setProperty("webdriver.chrome.driver", "c:\\Tools\\chromedriver.exe");
+      driver = new ChromeDriver(); //создается экземпляр класса ChromeDriver
+    } else if (browser == BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    } else if (browser == BrowserType.IE) {
+      driver = new InternetExplorerDriver();
+    }
+
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/edit.php");
