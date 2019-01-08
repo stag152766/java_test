6f28996955812bdbfe93880a8102250c33ddfdf6
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-01-08 09:23:51 +0700

    урок38 в процессе

--- sandBox/src/main/java/ru/testjava/sandBox/Primes.java
+++ sandBox/src/main/java/ru/testjava/sandBox/Primes.java
@@ -2,23 +2,51 @@ package ru.testjava.sandBox;
 
 public class Primes {
 
-  public static boolean isPrime(int n) { //true простое
+  public static boolean isPrime(int n) {
     for (int i = 2; i < n ; i++) {
-      if (n % i == 0) { //число делится без остатка
+      if (n % i == 0) {
       return false;
       }
     }
     return true;
   }
 
-  //чикл for может быть переписан в другой вид
+  //эквивалентная форма цикла for
   public static boolean isPrimeWhile(int n) {
     int i = 2;
     while (i < n) {
       if (n % i == 0) {
         return false;
       }
-      i++; //на каждой итерации цикла увеличиваем переменную на 1, выполнять эти действия будем пока i = n
+      i++;
+    }
+    return true;
+  }
+
+  public static boolean isPrime(long n) {
+    for (long i = 2; i < n ; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast(int n) {
+    for (int i = 2; i < n / 2; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrime2Fast(int n) {
+    int m = (int) Math.sqrt(n);
+    for (int i = 2; i < m / 2; i++) {
+      if (n % i == 0) {
+        return false;
+      }
     }
     return true;
   }
--- sandBox/src/test/java/ru/testjava/sandBox/PrimeTests.java
+++ sandBox/src/test/java/ru/testjava/sandBox/PrimeTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class PrimeTests {
 
-  @Test
+  @Test(enabled = false)
   public void testPrimes() {
     Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE)); //Integer.MAX_VALUE - простое число
   }
@@ -13,5 +13,17 @@ public class PrimeTests {
   @Test
   public void testNonPrimes() {
     Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2)); //Integer.MAX_VALUE - простое число
+  }
+
+
+  @Test(enabled = false)
+  public void testPrimesLong() {
+    long n = Integer.MAX_VALUE; //значение переменной будет преобразовано в тип long
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test
+  public void testPrimesFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
   }
 }
