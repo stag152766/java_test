e5806888be4321d90b7b916600b23e6df8890273
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-01-16 18:24:26 +0700

    урок44 завершен

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/appmanager/GroupHelper.java
@@ -71,7 +71,8 @@ public class GroupHelper extends HelperBase {
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
     for (WebElement element: elements) {
       String name = element.getText();
-      GroupData group = new GroupData(name, null,null);
+      String id = element.findElement(By.tagName("input")).getAttribute("value");  //откуда взять id ищем внутри одного элемента другой
+      GroupData group = new GroupData(id, name, null,null); //GroupHelper должен откуда то идентификатор извлекать, и передавать его в качестве параметра в конструктор
       groups.add(group);
     }
       return groups;
--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/GroupModificationTests.java
@@ -18,21 +18,16 @@ public class GroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData("test1", "test2", "test3"); //добавили локальную переменную
+    GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1", "test2", "test3"); //добавили локальную переменную
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size());
 
-    //модифицируем старые списки, чтобы предсказать ожидаемый результат
+    before.remove(before.size() -1);
+    before.add(group);
 
-    before.remove(before.size() -1); //удаляли последний элемент
-    before.add(group); //вместо него добавим тот, который должен появится после модификации
-
-
-    //добавим сравнение множеств
-
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<>(after)); //преобразуем списки в множества
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
