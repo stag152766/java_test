f728dfa05b1341cc1f4b6cf50913d2b08d91ba94
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-01-18 19:09:59 +0700

    урок 47 в процессе

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.testjava.addressbook.modul.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -17,7 +17,7 @@ public class GroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1", "test2", "test3"); //добавили локальную переменную
+    GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
@@ -26,8 +26,10 @@ public class GroupModificationTests extends TestBase{
 
     before.remove(before.size() -1);
     before.add(group);
-
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
