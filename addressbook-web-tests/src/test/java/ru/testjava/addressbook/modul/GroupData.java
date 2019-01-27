9d82b2b470cd0a8ab21edba5fed112e8f1472cc5
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-01-27 14:06:23 +0700

    update матчасти

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/modul/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/modul/GroupData.java
@@ -2,8 +2,6 @@ package ru.testjava.addressbook.modul;
 
 //вспомогательный класс, который описывает объект типа группа
 
-import java.util.Objects;
-
 public class GroupData {
   private int id = Integer.MAX_VALUE;
   private String name;
--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/modul/Groups.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/modul/Groups.java
@@ -1,7 +1,6 @@
 package ru.testjava.addressbook.modul;
 
 import com.google.common.collect.ForwardingSet;
-
 import java.util.HashSet;
 import java.util.Set;
 
