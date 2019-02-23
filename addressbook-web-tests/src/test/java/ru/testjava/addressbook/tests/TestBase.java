c625de5847c7fe281d1c42e1eb3ead0c9b8ff28b
stag152766 <45874547+stag152766@users.noreply.github.com>
2019-02-23 17:01:38 +0700

    урок 68 идет

--- addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/testjava/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.testjava.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp()  {
